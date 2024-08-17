659cfbad9d3a3e55929cfdcba32b20c60e180900
ChattawasK <hoshi.d2p@gmail.com>
2024-08-17 10:22:16 +0700

    add line liff

--- src/app/registor/registor.component.ts
+++ src/app/registor/registor.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 import Swal from 'sweetalert2'
+import liff from '@line/liff';
 
 @Component({
   selector: 'app-registor',
@@ -15,12 +16,45 @@ export class RegistorComponent implements OnInit {
     ])
   });
 
+  title : string | null = 'angular-line-login';
+  idToken : string | null = '';
+  displayName : string | null = '';
+  pictureUrl : string | undefined = '';
+  statusMessage : string | undefined = '';
+  userId : string | null = '';
+
   constructor() { }
 
-  ngOnInit() {
-
+  ngOnInit(): void {
+    this.initLine();
   }
 
+  initLine(): void {
+    liff.init({ liffId: '2005807746-xE4mZyWk' }, () => {
+      if (liff.isLoggedIn()) {
+        this.runApp();
+      } else {
+        liff.login();
+      }
+    }, err => console.error(err));
+  }
+
+  runApp(): void {
+    const idToken = liff.getIDToken();
+    this.idToken = idToken;
+    liff.getProfile().then(profile => {
+      console.log(profile);
+      this.displayName = profile.displayName;
+      this.pictureUrl = profile.pictureUrl;
+      this.statusMessage = profile.statusMessage;
+      this.userId = profile.userId;
+    }).catch(err => console.error(err));
+  }
+
+  logout(): void {
+    liff.logout();
+    window.location.reload();
+  }
   onSubmit(f: NgForm) {
     console.log(f.value); // { first: '', last: '' }
     console.log(f.valid); // false
